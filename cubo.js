e95ca20cdd0d8ed1dc3f6c27b8715b2d89ca4348
Jose14munozz <165930326+Jose14munozz@users.noreply.github.com>
2024-04-04 15:07:35 -0600

    Add files via upload

--- cubo.js
+++ cubo.js
@@ -1,18 +1,21 @@
-function updateClock() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    
-    document.querySelector('.hour-face').textContent = hours;
-    document.querySelector('.minute-face').textContent = minutes;
-    document.querySelector('.second-face').textContent = seconds;
-    
-    const rotationDegrees = [360 / 12 * now.getHours(), 360 / 60 * now.getMinutes(), 360 / 60 * now.getSeconds()];
-    const cube = document.querySelector('.cube');
-    cube.style.transform = `rotateX(30deg) rotateY(90deg) rotateZ(${rotationDegrees.join('deg,')}deg)`;
-  }
-  
-  updateClock();
-  setInterval(updateClock, 1000);
-  
+function date_time(id) {
+    date = new Date;
+    h = date.getHours();
+    if (h < 10) {
+        h = "0" + h;
+    }
+    m = date.getMinutes();
+    if (m < 10) {
+        m = "0" + m;
+    }
+    s = date.getSeconds();
+    if (s < 10) {
+        s = "0" + s;
+    }
+    document.getElementById("s").innerHTML = '' + s;
+    document.getElementById("m").innerHTML = '' + m;
+    document.getElementById("h").innerHTML = '' + h;
+    setTimeout('date_time("' + "s" + '");', '1000');
+    return true;
+}
+window.onload = date_time('s');
